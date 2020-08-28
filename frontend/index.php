31cd8937edc251f165a4d88b8603af106a56a04f
Pippyh <41901605+Pippyh@users.noreply.github.com>
2020-08-28 15:02:24 +0100

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -8,6 +8,7 @@ $mustache = new Mustache_Engine(['loader' => $mloader]);
 
 $names = ["Pippy", "Peter", "Mark", "Mary"];
 $data = [];
+$data = ['sport'] = 'tennis';
 
 foreach ($names as $thisname) {
     echo "<p>Hello $thisname</p>";
