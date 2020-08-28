b025e50c0f81c6b93fe26dd9cec30d4e00d5e2ed
Pippyh <41901605+Pippyh@users.noreply.github.com>
2020-08-28 14:58:06 +0100

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -8,14 +8,6 @@ $mustache = new Mustache_Engine(['loader' => $mloader]);
 
 $names = ["Pippy", "Peter", "Mark", "Mary"];
 
-echo "<p>do you like big monsters?</p>
-
-<h1>well you're going to</h1>
-
-<p>maybe</p>
-
-<h2>bibble bye</h2>";
-
 foreach ($names as $thisname) {
     echo "<p>Hello $thisname</p>";
 }
